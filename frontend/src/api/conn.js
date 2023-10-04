6fc1a97d9d883fc8e83dea021e84db2038f9fdf7
Woody Umstead <76594506+woodumstead@users.noreply.github.com>
2023-10-03 21:02:47 -0700

    Update conn.js

--- frontend/src/api/conn.js
+++ frontend/src/api/conn.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://w3-ticketing-n4bb.onrender.com";
 const apiConn = axios.create({baseURL: BASE_URL});
 
 export default apiConn;
